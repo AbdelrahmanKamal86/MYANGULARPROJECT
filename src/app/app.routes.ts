50a30e982a0655b2f3f620259783e99b4f7338c8
AbdelrahmanKamal86 <elgenteel3@gmail.com>
2025-11-19 14:03:43 +0200

    add ngIf

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,5 @@
 import { Routes } from '@angular/router';
-import { Female } from './female/female';
-import { Child } from './child/child';
-import { Continar } from './continar/continar';
-import { Homepage } from './homepage/homepage';
 
 
 export const routes: Routes = [
-  {path:'homepage',component:Homepage},
-  {path:'',component:Homepage},
-  {path:"female",component:Female},
-  {path:'child', component:Child},
-  {path:'continar',component:Continar},
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Aboutus } from "./aboutus/aboutus";
+import { Aboutus } from "./componnants/aboutus/aboutus";
+import { Navbar } from "./componnants/navbar/navbar";
+import { Footer } from "./componnants/footer/footer";
+import { Products } from "./componnants/products/products";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Aboutus],
+  imports: [RouterOutlet, Aboutus, Navbar, Footer, Products],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('firstapp');
-    isTrue=false;
-    onClick(){
-      alert('اهلا وسهلا');
-    }
 }
